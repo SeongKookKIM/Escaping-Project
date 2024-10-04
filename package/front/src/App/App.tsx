ecf4aff16aac32b2e58ef256a55797dc63bee0b5
SeongKookKIM <ksk8646@naver.com>
2024-10-04 22:45:07 +0900

    Feat.Scss설치

--- package/front/src/App/App.tsx
+++ package/front/src/App/App.tsx
@@ -1,27 +1,14 @@
-import { useState } from "react";
-
-import "./App.module.scss";
-import Home from "../Pages/Home/Home";
-import Test from "../Pages/Test/Test";
+import Home from "../Pages/\bHome/Home";
+import "./App.scss";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
-  const [isLogin, setIsLogin] = useState<boolean>(false);
-
-  const handlerLogin = () => {
-    setIsLogin(!isLogin);
-  };
-
   return (
-    <div>
-      <button type="button" onClick={handlerLogin}>
-        {isLogin ? "Logout" : "Login"}
-      </button>
-      <div className="module-test">
-        <p>모듈 테스트</p>
-      </div>
-      <Home />
-      <Test />
-    </div>
+    <main>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </main>
   );
 }
 
---
+++ package/front/src/Pages/Home/Home.tsx
@@ -0,0 +1,12 @@
+import style from "./Home.module.scss";
+
+function Home() {
+  return (
+    <div className={style.home}>
+      <p>test</p>
+      <p>testsdas</p>
+    </div>
+  );
+}
+
+export default Home;
