0bbfa3fef8bc1de63e1b1c3e42cd75c9fc843411
SeongKookKIM <ksk8646@naver.com>
2024-10-10 22:59:40 +0900

    Feat.RoomModel 생성

--- package/front/src/Entries/Rooms/RoomsModel.ts
+++ package/front/src/Entries/Rooms/RoomsModel.ts
@@ -2,7 +2,7 @@ import { wordsType } from "../Answer/AnswerModel";
 
 export const roomsModel: wordsType[] = [
   { words: "", navigation: "room1", entryBtnName: "1번방 입장하기" },
-  { words: "context", navigation: "room2", entryBtnName: "2번방 입장하기" },
+  { words: "virtualdom", navigation: "room2", entryBtnName: "2번방 입장하기" },
   { words: "csr", navigation: "room3", entryBtnName: "3번방 입장하기" },
   { words: "usecallback", navigation: "room4", entryBtnName: "4번방 입장하기" },
   { words: "hoisting", navigation: "room5", entryBtnName: "5번방 입장하기" },
--- package/front/src/Pages/Room/Room.tsx
+++ package/front/src/Pages/Room/Room.tsx
@@ -1,7 +1,20 @@
-import React from "react";
+import style from "./Room.module.scss";
+import Header from "../../Widgets/Header/Header";
+import { useParams } from "react-router-dom";
+import { useEffect } from "react";
 
 function Room() {
-  return <div>루우움</div>;
+  const roomNumber = useParams();
+
+  useEffect(() => {
+    console.log(roomNumber.id);
+  }, []);
+
+  return (
+    <div className={style.room}>
+      <Header backBtn={true} />
+    </div>
+  );
 }
 
 export default Room;
