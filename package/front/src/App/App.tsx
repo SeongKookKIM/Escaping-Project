4170e00e4910610d2db2fb6f632128a3765f9fe9
SeongKookKIM <ksk8646@naver.com>
2024-10-15 17:25:40 +0900

    Feat.Loading Page 생성

--- package/front/src/App/App.tsx
+++ package/front/src/App/App.tsx
@@ -1,21 +1,26 @@
-import Home from "../Pages/\bHome/Home";
-import Admission from "../Pages/Admission/Admission";
-import Escape from "../Pages/Escape/Escape";
-import Room from "../Pages/Room/Room";
-import Rooms from "../Pages/Rooms/Rooms";
-import "./App.scss";
+import { lazy, Suspense } from "react";
 import { Route, Routes } from "react-router-dom";
+import Home from "../Pages/\bHome/Home";
+const Admission = lazy(() => import("../Pages/Admission/Admission"));
+const Escape = lazy(() => import("../Pages/Escape/Escape"));
+const Room = lazy(() => import("../Pages/Room/Room"));
+const Rooms = lazy(() => import("../Pages/Rooms/Rooms"));
+
+import "./App.scss";
+import Loading from "../Features/Loding/Loading";
 
 function App() {
   return (
     <main>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/admission" element={<Admission />} />
-        <Route path="/rooms" element={<Rooms />} />
-        <Route path=":id" element={<Room />} />
-        <Route path="/escape" element={<Escape />} />
-      </Routes>
+      <Suspense fallback={<Loading />}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/admission" element={<Admission />} />
+          <Route path="/rooms" element={<Rooms />} />
+          <Route path=":id" element={<Room />} />
+          <Route path="/escape" element={<Escape />} />
+        </Routes>
+      </Suspense>
     </main>
   );
 }
---
+++ package/front/src/Features/Loding/Loading.tsx
@@ -0,0 +1,11 @@
+import style from "./Loading.module.scss";
+
+function Loading() {
+  return (
+    <div className={style.loading}>
+      <p>Page Loading...</p>
+    </div>
+  );
+}
+
+export default Loading;
